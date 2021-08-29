e9f6c7d3f5f16b017c43bbc9a2d945a163208d43
MelnykVL <w.vladyslavmelnyk@gmail.com>
2021-08-29 14:04:44 +0300

    create Student model

---
+++ src/main/java/com/melnyk/springexample/model/Student.java
@@ -0,0 +1,62 @@
+package com.melnyk.springexample.model;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Student {
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDate dob;
+
+    public Student() {
+    }
+
+    public Student(String name, String email, LocalDate dob) {
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public int getAge() {
+        return Period.between(dob, LocalDate.now()).getYears();
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", dob=" + dob +
+                '}';
+    }
+}
