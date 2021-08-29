01e085277edb8b0640b994d9ba823787c4080278
MelnykVL <w.vladyslavmelnyk@gmail.com>
2021-08-29 17:20:55 +0300

    add update method to StudentService.java

---
+++ src/main/java/com/melnyk/springexample/controller/StudentController.java
@@ -0,0 +1,37 @@
+package com.melnyk.springexample.controller;
+
+import com.melnyk.springexample.model.Student;
+import com.melnyk.springexample.service.StudentService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class StudentController {
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping(path = "list")
+    public List<Student> list() {
+        return studentService.list();
+    }
+
+    @PostMapping(path = "item")
+    public List<Student> add(@RequestBody Student student) {
+        studentService.add(student);
+        return studentService.list();
+    }
+
+    @DeleteMapping(path = "item/{studentId}")
+    public void delete(@PathVariable Long studentId) {
+        studentService.delete(studentId);
+    }
+
+    @PutMapping(path = "item")
+    public void update(@RequestBody Student student) {
+        studentService.upd
+    }
+}
--- src/main/java/com/melnyk/springexample/service/StudentService.java
+++ src/main/java/com/melnyk/springexample/service/StudentService.java
@@ -5,6 +5,7 @@ import com.melnyk.springexample.repository.StudentRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -24,5 +25,23 @@ public class StudentService {
 
     public void delete(Long studentId) {
         studentRepository.deleteById(studentId);
+    }
+
+    public void update(Student student) {
+        Optional<Student> row = studentRepository.findById(student.getId());
+
+        if (row.isPresent()) {
+            Student item = row.get();
+
+            if (!student.getName().isEmpty()) {
+                item.setName(student.getName());
+            }
+
+            if (!student.getEmail().isEmpty()) {
+                item.setEmail(student.getEmail());
+            }
+
+            studentRepository.save(item);
+        }
     }
 }
